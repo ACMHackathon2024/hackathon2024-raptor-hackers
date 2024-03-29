ee26056fe95a111f7bb85a62972bcfb8d666aff8
Lucas-Liona <165396297+Lucas-Liona@users.noreply.github.com>
2024-03-29 15:39:25 -0400

    Add files via upload
    not working currently

--- from cryptography.py
+++ from cryptography.py
@@ -5,18 +5,45 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-password = b"password"
-salt = b"salt"
-#key1 = Fernet(Fernet.generate_key())
-kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=480000,)
-key2 = base64.urlsafe_b64encode(kdf.derive(password))
-f = Fernet(key2)
-token = f.encrypt(b"Secret message!")
+filePtr = open('file.txt', 'w')
+filePtr.write("MY SECRET MESSAGE")
+filePtr.close()
 
-print(f.decrypt(token))
+def encrypt():
 
+    fileName = input("Please Enter File Name (file.txt): ")
+    file = open(fileName, 'r')
+    FILECONTENTS = file.read()
+    file.close()
 
-#key3 = Fernet(Fernet.generate_key())
-#f2 = MultiFernet([key3, key1, key2])
-#rotated = f2.rotate(token)
-#f2.decrypt(rotated)
+    try:
+        pswd = input("Please Enter Password for Encryption: ")
+        password = pswd.encode('utf-8')
+        salt = b"salt"
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=480000,)
+        key2 = base64.urlsafe_b64encode(kdf.derive(password))
+        f = Fernet(key2)
+        token = f.encrypt(FILECONTENTS.encode('utf-8'))
+
+        print("Token: " + token)
+        return(token)
+
+    except:
+        print("Error Encrypting")
+
+def decrypt(token):
+
+    try:
+        pswd = input("Please Enter Password for Encryption: ")
+        password = pswd.encode('utf-8')
+        salt = b"salt"
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=480000,)
+        key2 = base64.urlsafe_b64encode(kdf.derive(password))
+        f = Fernet(key2)
+
+        return(f.decrypt(token).decode('utf-8'))
+    
+    except:
+        print("Incorrect Password")
+
+print(decrypt(encrypt()))
